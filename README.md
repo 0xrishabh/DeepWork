97c169321004a46176c82b86e2c480e5929b1ec6
Rishabh Shukla <rishabhcodershukla@gmail.com>
2023-06-16 19:40:34 +0530

    Add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Why?
+
+It can help you with focusing on specific topics instead of opening 5000 tabs at once and jumping in between.
+
+# Install
+
+1. Clone this repo
+2. Navigate to chrome://extensions
+3. Expand the Developer dropdown menu and click “Load Unpacked Extension”
+4. Navigate to the local folder containing the extension’s code and click Ok
+5. Assuming there are no errors, the extension should load into your browser
+
+# Features:
+
+* A switch button by which you can open or close the Focus mode.
+* In Focus mode, you are allowed to open only `5` tabs per window.
--- popup.js
+++ popup.js
@@ -3,15 +3,25 @@ let MAX_TABS_COUNT = 3;
 
 chrome.storage.local.get('maxTabsCount', function(result) {
   let maxTabsCount = result["maxTabsCount"];
-  document.getElementsByClassName('switch')[0].innerHTML = maxTabsCount == MAX_TABS_COUNT ? "OFF" : "ON";
+  updateUI(maxTabsCount);
 });
 
 document.getElementsByClassName('switch')[0].addEventListener('click', function(e) {
-    chrome.storage.local.get('maxTabsCount', function(result) {
+  getValue(function(result) {
         let maxTabsCount = result["maxTabsCount"];
         let switchValue = maxTabsCount == MAX_TABS_COUNT ? 10e6 : MAX_TABS_COUNT;
-        chrome.storage.local.set({ 'maxTabsCount': switchValue }, function() {});
-        document.getElementsByClassName('switch')[0].innerHTML = maxTabsCount == MAX_TABS_COUNT ? "OFF" : "ON";
+        updateUI(switchValue);
+        setValue(switchValue, function() {});
     });
     
 });
+
+function updateUI(maxTabsCount) {
+  document.getElementsByClassName('switch')[0].textContent = maxTabsCount == MAX_TABS_COUNT ? "OFF" : "ON";
+}
+function getValue(callback){
+    chrome.storage.local.get('maxTabsCount', callback);
+}
+function setValue(value, callback){
+    chrome.storage.local.set({ 'maxTabsCount': value }, callback);
+}
